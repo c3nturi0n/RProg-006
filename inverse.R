18c5820764d2e43a0a2c2cf7545666cb8ca8e11f
c3nturi0n <c3nturi0n@users.noreply.github.com>
2014-08-25 05:09:33 +0530

    Update inverse.R

--- inverse.R
+++ inverse.R
@@ -17,7 +17,7 @@ makeMatrix <- function(x = matrix()) {
 cacheinv <- function(x, ...) {
         m <- x$getinv()
         if(!is.null(m)) {
-                message("getting cached data")
+                message("getting cached data")     # when the operation is already done for that matrix in past
                 return(m)
         }
         data <- x$get()
@@ -28,6 +28,7 @@ cacheinv <- function(x, ...) {
 
 
 # tested using following commands in R console:
-# a <- matrix(1:4,2,2)
-# b <- makeMatrix(a)
-# cacheinv(b)
+# a <- matrix(1:4,2,2)        to create an invertible matrix
+# b <- makeMatrix(a)          using makeMatrix function
+# cacheinv(b)                 finding inverse of matrix
+# cacheinv(b)                 to check if cache value is returned or new value is computed
